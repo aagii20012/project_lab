30a75ec414ca56b812458b2e21525bae671d904d
altangerel <aagii200148@gmail.com>
2023-05-21 14:42:42 +0800

    diploma init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: 'postgres',
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: 'test',
   password: '99201012',
   port: 5432,
-})
+});
 
 const app = express();
 
@@ -17,28 +17,14 @@ app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/test/give',(req,res)=>{
-    pool.query('SELECT * FROM users', (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.status(200).json(results.rows)
-    })
-})
+app.get('/test/give', (req, res) => {
+  res.status(200).json('hello world');
+});
 
-app.get('/test/save',(req,res)=>{
-    pool.query('INSERT INTO users (email) VALUES ($1)', [req.body.email],(error, results) => {
-        if (error) {
-            return res.send({error: error})
-        }
-        res.send(results.rows)
-    })
-})
-
-
-
-
+app.get('/test/save', (req, res) => {
+  res.status(200).json('hello world');
+});
 
 app.listen(3000, () => {
-    console.log(`Example app listening on port ${3000}`);
-  });
+  console.log(`Example app listening on port ${3000}`);
+});
